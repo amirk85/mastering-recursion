87714c4d1cbed4b326d730bf46c56ac76ce3828b
Amir Khan <khanamir8589@gmail.com>
2024-03-19 14:56:06 +0530

    updates

--- array/quickSort.js
+++ array/quickSort.js
@@ -1,29 +1,29 @@
 const swap = (arr, f, s) => ([arr[f], arr[s]] = [arr[s], arr[f]]);
 
 function quickSort(arr, s = 0, e = arr.length - 1) {
-  if (arr.length <= 1) return arr;
+    if (arr.length <= 1) return arr;
 
-  const mid = Math.floor((s + e) / 2);
-  const pivot = arr[mid];
+    const mid = Math.floor((s + e) / 2);
+    const pivot = arr[mid];
 
-  let low = s;
-  let high = e;
+    let low = s;
+    let high = e;
 
-  while (low <= high) {
-    while (arr[low] < pivot) low++;
-    while (arr[high] > pivot) high--;
+    while (low <= high) {
+        while (arr[low] < pivot) low++;
+        while (arr[high] > pivot) high--;
 
-    if (low <= high) {
-      swap(arr, low, high);
-      low++;
-      high--;
+        if (low <= high) {
+            swap(arr, low, high);
+            low++;
+            high--;
+        }
     }
-  }
 
-  if (s < high) quickSort(arr, s, high);
-  if (low < e) quickSort(arr, low, e);
+    if (s < high) quickSort(arr, s, high);
+    if (low < e) quickSort(arr, low, e);
 
-  return arr;
+    return arr;
 }
 
 const result = quickSort([7, 2, 9, 1, 5, 4, 6, 3, 8]);
